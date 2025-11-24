4f02be704b8fcc037abc3ee484bae99c25f42968
EvezMemmedov <evezm254@gmail.com>
2025-11-24 22:15:56 +0400

    Added

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""
+this module sum float a and float b
+"""
 def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats a and b
+    """
     return a + b

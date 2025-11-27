4efc8fe31eefe859f383f4961b363d77dcf16a35
EvezMemmedov <evezm254@gmail.com>
2025-11-27 22:40:01 +0400

    Added

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,13 +4,13 @@ Docstring for python_async_function.0-basic_async_syntax
 """
 
 
-import asyncio, random
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
     Docstring for wait_random
-    
     :param max_delay: Description
     :type max_delay: int
     :return: Description

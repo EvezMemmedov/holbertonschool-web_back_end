7b25184b314f7944d0d4d73264d31a5c9169a6e8
Evez Memmedov <evezm254@gmail.com>
2025-11-28 10:42:42 +0400

    Added

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,12 +3,12 @@
 execute multiple async tasks
 """
 
-
+from typing import List
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     return sort delay
     """

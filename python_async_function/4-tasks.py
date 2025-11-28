8234e00097dad085a1a3ac952225a4b4caaa6885
Evez Memmedov <evezm254@gmail.com>
2025-11-28 10:34:04 +0400

    Added

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+execute multiple async tasks
+"""
+
+
+import asyncio
+task_wait_random = __import__('1-concurrent_coroutines').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> int:
+    """
+    return sort delay
+    """
+    tasks = []
+
+    for _ in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+        delay = await asyncio.gather(*tasks)
+        return sorted(delay)

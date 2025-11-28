82469f6bff77360a902a63d6f48a1c1f74344e36
Evez Memmedov <evezm254@gmail.com>
2025-11-28 10:41:18 +0400

    Added

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> int:
+async def task_wait_n(n: int, max_delay: int) -> list[float]:
     """
     return sort delay
     """

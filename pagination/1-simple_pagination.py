08611b01819f28d9a631e7728d5e6611cf3dcfc9
EvezMemmedov <evezm254@gmail.com>
2025-12-09 20:45:15 +0400

    Added

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -3,7 +3,7 @@
 import csv
 from typing import List, Tuple
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page, page_size):
     """Return start and end indexes"""
     start = (page - 1) * page_size
     end = start + page_size

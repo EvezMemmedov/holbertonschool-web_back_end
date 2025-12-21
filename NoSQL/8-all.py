d40b84d45ad80fbb446efd9097f7518ebe91ccf3
Evez Memmedov <evezm254@gmail.com>
2025-12-21 23:41:48 +0400

    Added

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+"""
+Module 8-all
+Contains a function that lists all documents in a MongoDB collection.
+"""
+
+
 def list_all(mongo_collection):
-    """Lists all documents in a MongoDB collection"""
+    """
+    Lists all documents in a MongoDB collection.
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        A list of documents in the collection.
+        Returns an empty list if the collection is empty.
+    """
     return list(mongo_collection.find())

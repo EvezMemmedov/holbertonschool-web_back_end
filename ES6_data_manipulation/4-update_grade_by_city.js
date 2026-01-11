7b6f2ceebc8b852de635fc85c2612cb10ef902ac
Evez Memmedov <evezm254@gmail.com>
2026-01-11 17:46:11 +0000

    Added

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,13 @@
-export default function updateStudentGradeByCity(student, city, newGrades) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter((student) => student.location => city)
+    .filter((student) => student.location === city)
     .map((student) => {
       const gradeObj = newGrades.find(
-        grade => grade.studentId === student.id
+        (grade) => grade.studentId === student.id
       );
       return {
         ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A'
+        grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
 }

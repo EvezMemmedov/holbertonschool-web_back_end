10627d06c8c1afc41ef36e715fe15c21bf3bc092
Evez Memmedov <evezm254@gmail.com>
2026-01-17 19:35:02 +0000

    Added

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,11 @@
 import { uploadPhoto, createUser } from './utils.js';
+
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} {user.lastName}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      consolo.log('Signup system offline');
+      console.log('Signup system offline');
     });
 }

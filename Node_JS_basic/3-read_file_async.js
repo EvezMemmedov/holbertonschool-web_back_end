eb0ad54d8be397627dc5e0759d65821b9263a36f
EvezMemmedov <evezm254@gmail.com>
2026-01-22 21:20:40 +0400

    Added

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,33 +3,42 @@ const fs = require('fs');
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
-      if(err) {
+      if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
+
       const lines = data
         .split('\n')
-        .filter((line) =>line.trim() !== '');
+        .filter((line) => line.trim() !== '');
 
       const students = lines.slice(1);
+
       console.log(`Number of students: ${students.length}`);
+
       const fields = {};
+
       students.forEach((line) => {
         const parts = line.split(',');
         const firstName = parts[0];
         const field = parts[3];
-      if(!fields[filed]) {
-        fields[field] = [];
-      }
-      fileds[filed].push(firstName);
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstName);
       });
-      Object.keys(fileds).forEach((filed) => {
+
+      Object.keys(fields).forEach((field) => {
         console.log(
-          `Number of students in ${filed}: ${fileds[filed].length}. List: ${fileds[filed].join(', ')}`,
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
         );
       });
+
       resolve();
     });
   });
 }
+
 module.exports = countStudents;

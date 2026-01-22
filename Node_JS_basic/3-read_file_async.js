3f1a425002b3a500f0f565651e7b53157e51c2c8
Evez Memmedov <evezm254@gmail.com>
2026-01-22 15:26:56 +0000

    Added

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -9,7 +9,7 @@ function countStudents(path) {
       }
       const lines = data
         .split('\n')
-        .filter((line) =>line.trim( !== ''));
+        .filter((line) =>line.trim() !== '');
 
       const students = lines.slice(1);
       console.log(`Number of students: ${students.lenght}`);
